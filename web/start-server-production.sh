0424b828687689a7dc9d500ee04cabcf297d8708
Danying Shao <danying.shao@gmail.com>
2020-07-01 22:47:19 -0400

    increase timeout limit

--- web/start-server-production.sh
+++ web/start-server-production.sh
@@ -6,4 +6,4 @@ python manage.py migrate
 
 echo "from django.contrib.auth.models import User; User.objects.count()>0 or User.objects.create_superuser('admin', 'asite.ip@gmail.com', 'admin')" | python manage.py shell
 
-/usr/local/bin/gunicorn aip_project.wsgi:application -w 2 -b :8000
+/usr/local/bin/gunicorn aip_project.wsgi:application --timeout 300 -w 2 -b :8000
