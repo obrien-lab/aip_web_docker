f9d539daa1d7b330726cd20ed3c941f3cd479fe0
Danying Shao <danying.shao@gmail.com>
2021-02-28 08:57:34 -0500

    bug fix

--- web/aipservice/aip.py
+++ web/aipservice/aip.py
@@ -829,7 +829,7 @@ def generate_asite_profiles(frag_min, frag_max, offsets, scratch, folder, three_
     with open(os.path.join(folder, 'A-site_profiles_codon.tab'), 'w') as asite_file:
         for gene in asite_profiles:
             reads_codon = np.add.reduceat(asite_profiles[gene], np.arange(0, dict_len[gene], 3))
-            asite_file.write(gene + '\t' + str(dict_len[gene]/3) + '\t' + ','.join(map(str, reads_codon)) + '\n')
+            asite_file.write(gene + '\t' + str(int(dict_len[gene]/3)) + '\t' + ','.join(map(str, reads_codon)) + '\n')
     
 
 # Using cutoffs for all possible frag sizes
